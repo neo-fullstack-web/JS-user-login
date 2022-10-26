8d6c0b7d691f60dfb8247d1da679943d7aca08f4
Martin Rodriguez <neomartinr@gmail.com>
2022-10-26 18:46:45 -0300

    login structure rdy

---
+++ js/users.js
@@ -0,0 +1,48 @@
+const users = [
+    {
+        name: `John Doe`,
+        email: `johndoe@gmail.com`,
+        password: `alfabeta`,
+        active: true,
+        role: `ADMIN_ROLE`
+    },
+    {
+        name: `Jose Perez`,
+        email: `joseperez@gmail.com`,
+        password: `1234`,
+        active: true,
+        role: `USER_ROLE`
+    },
+    {
+        name: `Usuario Inactivo`,
+        email: `inactivo@gmail.com`,
+        password: `1234`,
+        active: false,
+        role: `USER_ROLE`
+    },
+    {
+        name: `Maria Gimenez`,
+        email: `mariagimenez@gmail.com`,
+        password: `1234`,
+        active: true,
+        role: `USER_ROLE`
+
+    },
+    {
+        name: `Eusebio Cobos`,
+        email: `eusebio@gmail.com`,
+        password: `1234`,
+        active: false,
+        role: `USER_ROLE`
+    },
+    {
+        name: `Usuario Registrado`,
+        email: `usuario@gmail.com`,
+        password: `1234`,
+        active: true,
+        role: `USER_ROLE`
+    }
+];
+
+
+
